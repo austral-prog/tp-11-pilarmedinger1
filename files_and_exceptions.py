bf76fc493bccee4d6d54db080b6eefe8fa59efb7
pilarmedinger1 <pmedinger@mail.austral.edu.ar>
2025-07-11 11:18:51 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -1,17 +1,40 @@
 def read_file_to_dict(filename):
-    """Lee un archivo de ventas donde cada venta es producto:valor_de_venta;... y agrupa los valores por producto en una lista.
+    ventas_por_producto = {} #diccionario vacio pra guardar cada producto con su lista de ventas
+    
+    try:
+        with open(filename, 'r') as file: #abre el archivo en modo lectura, y le pone el nombre file
+            linea = file.readline () #lee linea del archivo, hay una sola linea asique alcanza
+            lista_entrada = linea.split(";") #corta la linea cada vez que hay un ";"
+            #entradas es una lista ahora con items asi ['producto1': 100]
 
-    :param filename: str - nombre del archivo a leer.
-    :return: dict - diccionario con listas de montos por producto.
-    :raises: FileNotFoundError - si el archivo no existe.
-    """
-    return {}
+            for entrada in lista_entrada: #recorre cada entra de tipo product0:valor
+                if entrada == '': #si la entrada esta vacia la saltea
+                    pass
+                #separamos el prodcuto del valor
+                producto, valor = entrada.split(":") #separa en dos variables
+                #producto1:100, se convierte en producto=producto1 y valor=100 (ambos son texto)
+                valor = float(valor) #cpnvierte el valor a nro (100.0)
+
+                #si el producto no esta en el diccionario, lo agrego con una lista vacia para depsues rellenarla con los numeros
+                if producto not in ventas_por_producto:
+                    ventas_por_producto[producto] = []
+
+                #agregamos el valor a la lista correspondiente
+                ventas_por_producto[producto].append(valor)
+        
+        #diccionario final con todos los productos y sus listas de ventas
+        return ventas_por_producto
+    #si no se encuentra el archivo
+    except FileNotFoundError:
+        raise FileNotFoundError(f"El archivo '{filename}' no existe.")
+
 
 
 def process_dict(data):
-    """Para cada producto, imprime el total de ventas y el promedio, en el orden natural del diccionario.
+    for producto, ventas in data.items():
+        total = sum(ventas)
+        promedio = total / len(ventas)
+        print (f"{producto}: ventas totales ${total:.2f}, promedio ${promedio:.2f}") #:.2f es para q sean 2 cofras decimales
 
-    :param data: dict - diccionario a procesar.
-    :return: None
-    """
-    pass
+
+
